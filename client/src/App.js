ac87fda6e1fa2a9083bf3152c5f6be0ef7bbf2be
vibha2 <juhisahu2203@gmail.com>
2024-08-22 20:55:07 +0530

    app file

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Good Morning</div>;
 }
 
 export default App;
